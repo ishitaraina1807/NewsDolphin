e9a56a6ac3d18ca66e0ab4e09b8eb9fbc23c3f69
Mermaido <106395332+ishitaraina1807@users.noreply.github.com>
2023-07-24 19:48:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ export default function App() {
       <>
       <NavBar/>
         <Routes>
-          <Route exact path="/" element={<News key="general" pageSize={6} country="in" category="general" />} />
-          <Route exact path="/Business" element={<News key="business" pageSize={6} country="in" category="business" />} />
-          <Route exact path="/Health" element={<News key="health" pageSize={6} country="in" category="health" />} />
-          <Route exact path="/Sports" element={<News key="sports" pageSize={6} country="in" category="sports" />} />
-          <Route exact path="/Science" element={<News key="science" pageSize={6} country="in" category="science" />} />
-          <Route exact path="/Technology" element={<News key="technology" pageSize={6} country="in" category="technology" />} />
-          <Route exact path="/Entertainment" element={<News key="entertainment" pageSize={6} country="in" category="entertainment" />} />
+          <Route exact path="/" element={<News key="general" pageSize={15} country="in" category="general" />} />
+          <Route exact path="/Business" element={<News key="business" pageSize={15} country="in" category="business" />} />
+          <Route exact path="/Health" element={<News key="health" pageSize={15} country="in" category="health" />} />
+          <Route exact path="/Sports" element={<News key="sports" pageSize={15} country="in" category="sports" />} />
+          <Route exact path="/Science" element={<News key="science" pageSize={15} country="in" category="science" />} />
+          <Route exact path="/Technology" element={<News key="technology" pageSize={15} country="in" category="technology" />} />
+          <Route exact path="/Entertainment" element={<News key="entertainment" pageSize={15} country="in" category="entertainment" />} />
         </Routes>
       </>
     </div>
